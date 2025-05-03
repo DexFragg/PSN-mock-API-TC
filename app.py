c25f042c41227249311daa0b1926a74948ccf734
Game Craves <dex@gamecraves.com>
2025-05-03 15:24:34 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,40 +1,46 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import os
 
 app = Flask(__name__)
 CORS(app)
-
-@app.route('/')
-def home():
-    return {'message': 'PSNP Flask API is live!'}
 
 @app.route('/api/trophies/<username>')
 def get_trophies(username):
     return jsonify({
         "user": username,
-        "level": 350,
-        "xp_progress": 73,
+        "level": 421,
+        "xp_progress": 92,
+        "total_trophies": {
+            "platinum": 12,
+            "gold": 85,
+            "silver": 232,
+            "bronze": 894
+        },
         "games": [
             {
                 "title": "God of War Ragnarok",
-                "cover": "https://image.example.com/gowr.jpg",
-                "progress": 85,
+                "cover": "https://image.api.playstation.com/vulcan/ap/rnd/202211/1615/yjqOyDjTDXFaX6CMvH7nWZ2Q.png",
+                "progress": 87,
                 "trophies": {
                     "platinum": 1,
-                    "gold": 5,
+                    "gold": 3,
                     "silver": 10,
-                    "bronze": 20
-                },
-                "rarity": {
-                    "Ultra Rare": 2,
-                    "Rare": 8,
-                    "Common": 26
+                    "bronze": 22
+                }
+            },
+            {
+                "title": "Horizon Forbidden West",
+                "cover": "https://image.api.playstation.com/vulcan/ap/rnd/202202/2316/NvJkD1RB0EpjYdYysaaN3VyD.png",
+                "progress": 64,
+                "trophies": {
+                    "platinum": 0,
+                    "gold": 4,
+                    "silver": 12,
+                    "bronze": 18
                 }
             }
         ]
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
